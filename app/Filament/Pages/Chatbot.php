90732b153f7cab2bee6197c095ac751ea95506e7
Afif Al Fakri <afif.al.fakri@gmail.com>
2025-09-03 02:47:15 +0700

    Implement shield in chatbot and Update Seeder factory Company Properties

--- app/Filament/Pages/Chatbot.php
+++ app/Filament/Pages/Chatbot.php
@@ -3,10 +3,15 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 
 class Chatbot extends Page
 {
+    use HasPageShield;
+
+    
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.chatbot';
+    
 }
--- database/factories/CompanyPropertyFactory.php
+++ database/factories/CompanyPropertyFactory.php
@@ -17,10 +17,19 @@ class CompanyPropertyFactory extends Factory
      */
     public function definition(): array
     {
+        $options = [
+            'Gaji' => ['5.000.000', '7.000.000', '10.000.000', '15.000.000', '20.000.000'],
+            'Jam Kerja' => ['08:00 - 17:00', '09:00 - 18:00', '10:00 - 19:00'],
+            'Penempatan Kerja' => ['Jakarta', 'Bandung', 'Surabaya'],
+            'Asuransi Kesehatan' => ['BPJS Kesehatan', 'Asuransi Swasta'],
+            'Jenjang Karir' => ['Junior', 'Mid', 'Senior'],
+            'Fasilitas' => ['Laptop', 'Transportasi', 'Tunjangan Makan'],
+        ];
+
         return [
             'company_id' => Company::inRandomOrder()->first()->id,
-            'key' => fake()->word(),
-            'value' => fake()->word()
+            'key' => $key = fake()->randomElement(array_keys($options)),
+            'value' => fake()->randomElement($options[$key]),
         ];
     }
 }
